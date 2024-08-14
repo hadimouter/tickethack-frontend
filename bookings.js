e57964fa4d274d637a0b1834c6c091bbcc330fa3
Hadi <mouterdadi@gmail.com>
2024-08-14 20:41:33 +0200

    change vercel url

--- bookings.js
+++ bookings.js
@@ -1,5 +1,5 @@
 // Get bookings
-fetch('http://localhost:3000/bookings')
+fetch('https://tickethack-backend-seven-azure.vercel.app/bookings')
   .then(response => response.json())
   .then(data => {
     if (data.result) {
